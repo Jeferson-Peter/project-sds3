0c70ca1484d9674333186b62ace47925fd746866
Jeferson-Peter <jefersonpeter32@gmail.com>
2021-05-08 15:35:24 -0300

    Basic Static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      <div className="">
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://github.com/Jeferson-Peter" target="_blank" rel="noreferrer">Jeferson Peter</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+      </div>
+    );
+  }
+  
+  export default Footer;
